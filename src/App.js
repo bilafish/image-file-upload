daee29a4c84fff78be1e47f7f56c593e71edcc73
Jason <bilafish@gmail.com>
2021-01-21 15:45:05 +0800

    feat: show file type & file size

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import FileInput from "./components/FileInput.js";
 
 function App() {
   const [base64String, setBase64String] = useState("");
+  const [fileType, setFileType] = useState(null);
+  const [fileSize, setFileSize] = useState(null);
   console.log(base64String);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Image File Upload</h1>
-        <FileInput setBase64String={setBase64String} />
+        <FileInput
+          setBase64String={setBase64String}
+          setFileType={setFileType}
+          setFileSize={setFileSize}
+        />
+        {fileType && <p>File type: {fileType}</p>}
+        {fileSize && <p>File size: {Math.round(fileSize * 10) / 10} mb</p>}
         {base64String.length > 0 && (
           <img
-            src={`data:image/png;base64,${base64String}`}
+            src={`data:${fileType};base64,${base64String}`}
             alt="Uploaded image"
           />
         )}
--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,9 +1,13 @@
-const FileInput = ({ setBase64String }) => {
+const FileInput = ({ setBase64String, setFileType, setFileSize }) => {
   return (
     <form
       onChange={(e) => {
         let file = e.target.files[0];
+        console.log("File:", file);
         if (file) {
+          setFileType(file.type);
+          // Convert from bytes to megabytes
+          setFileSize(file.size / 1024 / 1024);
           const reader = new FileReader();
           reader.onload = (readerEvent) => {
             let binaryString = readerEvent.target.result;
